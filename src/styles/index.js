0b75321fd1023402087d75960bd848751cbd92cb
Jesse Katsumata <jesse.katsumata@gmail.com>
2018-12-17 21:56:08 +0900

    :art: apply lint

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,3 @@
 // styles/index.js
-export { default as app } from './appStyle';
-export { default as home } from './homeStyle';
+export { default as app } from './appStyle'
+export { default as home } from './homeStyle'
